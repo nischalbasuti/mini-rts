3a5f6d9773da3c605613d35d5ed3eb519283e36a
Nischal Basuti <nischalbasuti1@gmail.com>
2024-11-01 02:41:11 +0530

    make infantry sprites instead of custome gameobjects

--- src/InfantryUnit.ts
+++ src/InfantryUnit.ts
@@ -1,32 +1,24 @@
-import { GameObjectClass } from "kontra";
+import { Sprite, track } from "kontra";
 import { Player } from "./Player";
 import { Unit } from "./Unit";
 
 export class InfantryUnit extends Unit {
   constructor(player: Player, x: number, y: number) {
-    const gameObject = new Triangle({
+    const gameObject = Sprite({
       color: player.color,
       x,
       y,
       width: 20,
       height:40,
       anchor: { x: 0.5, y: 0.5 },
+      onDown: function (evt: any) {
+        console.log("clicked on infantry unit", evt, self);
+      }
     });
+
     super(100, 10, 10, gameObject);
-  }
-}
 
-class Triangle extends GameObjectClass {
-  constructor(properties: any) {
-    super(properties);
-  }
-
-  draw() {
-    this.context.fillStyle = this.color;
-    this.context.beginPath();
-    this.context.moveTo(0, 0);
-    this.context.lineTo(this.width, 0);
-    this.context.lineTo(this.width / 2, this.height);
-    this.context.fill();
+    const self = this;
+    track(gameObject)
   }
 }
